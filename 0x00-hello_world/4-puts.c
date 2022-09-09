0d29117ea093eb9c85c3ee3daa6652c38fa6cb04
Edwin Oghenetejiri <darealthej@gmail.com>
2022-09-08 20:53:26 -0700

    task4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main-serves as the entry point for the program
  *
- * Return: By default returns zero if no errors otherwise returns non-zero value.
+ * Return: returns zero if no errors otherwise returns non-zero value.
  */
 
 int main(void)
